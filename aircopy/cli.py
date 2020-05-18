b35d9ff0f5d40b46a448af41e04b6cb10422a819
st3107 <st3107@columbia.edu>
2020-05-18 17:55:23 -0400

    Doc: add doc

--- aircopy/cli.py
+++ aircopy/cli.py
@@ -1,12 +1,1 @@
 """The cli interface."""
-import aircopy.parser as parser
-import aircopy.database as database
-from airtable import Airtable
-
-
-def get_projecta_docs(projects: Airtable, people: Airtable, institutions: Airtable, add_info: dict, **options):
-    for page in projects.get_iter(**options):
-        for record in page:
-            database.denormalize_project(record, people, institutions, inplace=True)
-            yield parser.parse_project(record, add_info)
-
