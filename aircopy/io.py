245e538d41556b8c6fb42a50d7c116ec898ac74c
st3107 <st3107@columbia.edu>
2020-05-21 14:08:10 -0400

    BUG: fix yaml indent

--- aircopy/io.py
+++ aircopy/io.py
@@ -1,9 +1,13 @@
 from typing import Any
 
-import yaml
+from ruamel.yaml import YAML
+
+# regolith style yaml loader
+RG_LOADER = YAML()
+RG_LOADER.indent(mapping=2, sequence=4, offset=2)
 
 
-def write_docs_to_yaml(filename: str, content: Any) -> None:
+def write_docs_to_yaml(filename: str, content: Any, loader=RG_LOADER) -> None:
     """Write out the documents to yaml file.
 
     Parameters
@@ -13,7 +17,10 @@ def write_docs_to_yaml(filename: str, content: Any) -> None:
 
     content : Any
         The data that can be dumped by safe_dump.
+
+    loader : YAML
+        The loader to dump to yaml. It must have method 'dump'.
     """
     with open(filename, 'w') as f:
-        yaml.safe_dump(content, f)
+        loader.dump(content, f)
     return
--- aircopy/tests/test_io.py
+++ aircopy/tests/test_io.py
@@ -5,9 +5,9 @@ import aircopy.io as io
 
 
 def test_write_docs_to_yaml():
-    dct = {'k1': 'v1', 'k2': 'v2'}
+    dct = {'k1': ['v11', 'v12'], 'k2': 'v2'}
     with tempfile.TemporaryDirectory() as d:
         filename = Path(d).joinpath('test.yaml')
         io.write_docs_to_yaml(str(filename), dct)
         with filename.open('r') as f:
-            assert f.read() == "k1: v1\nk2: v2\n"
+            assert f.read() == "k1:\n  - v11\n  - v12\nk2: v2\n"
