76cc71f8f5bf215ca63bfab36ce26f04090a5994
st3107 <st3107@columbia.edu>
2020-05-21 16:40:36 -0400

    BLD: auto deploy documentation

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+coverage run -m pytest  # Run the tests and check for test coverage.
+coverage report -m  # Generate test coverage report.
+codecov  # Upload the report to codecov.
+flake8  # Enforce code style ('relaxed' line length limit is set in .flake8 config file).
+make -C docs html  # Build the documentation.
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]  # if change on master and not PR
+then
+  set -e
+  pip install doctr
+  doctr doctr deploy --built-docs docs
+fi
