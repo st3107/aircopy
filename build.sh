426c321c247f74b0e556f7ed78808d440734d284
st3107 <st3107@columbia.edu>
2020-05-21 17:25:53 -0400

    BLD: debug CI

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@ coverage report -m     # Generate test coverage report.
 codecov                # Upload the report to codecov.
 flake8                 # Enforce code style ('relaxed' line length limit is set in .flake8 config file).
 make -C docs html      # Build the documentation.
-if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_TAG" != "" ]; then # if
-  # change on master
-  # and
-  # not PR
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_TAG" != "" ]; then
+  # if a tag is pushed on master and not in PR, then deploy the docs
   set -e
   pip install doctr
   doctr deploy . --built-docs docs/build/html
