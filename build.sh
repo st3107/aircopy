6f98750bd89a18145ec490b746f5070a4151324f
st3107 <st3107@columbia.edu>
2020-05-21 16:51:48 -0400

    BLD: debug CI

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-coverage run -m pytest  # Run the tests and check for test coverage.
-coverage report -m  # Generate test coverage report.
-codecov  # Upload the report to codecov.
-flake8  # Enforce code style ('relaxed' line length limit is set in .flake8 config file).
-make -C docs html  # Build the documentation.
-if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]  # if change on master and not PR
-then
+coverage run -m pytest # Run the tests and check for test coverage.
+coverage report -m     # Generate test coverage report.
+codecov                # Upload the report to codecov.
+flake8                 # Enforce code style ('relaxed' line length limit is set in .flake8 config file).
+make -C docs html      # Build the documentation.
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then # if change on master and not PR
   set -e
   pip install doctr
-  doctr deploy --built-docs docs
+  doctr doctr deploy . --built-docs ./build/html
 fi
